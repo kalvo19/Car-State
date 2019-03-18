274f4e1759745e9960eb1b31e01c99c078a1395e
Marc Sanuy <marcsanuy@gmail.com>
2019-03-18 14:57:37 +0100

    login fixed

--- app/views/components/login.php
+++ app/views/components/login.php
@@ -1,4 +1,4 @@
-<div>
+<div class="welcome">
     <h4 class="info">Mantén el control de las tareas de tu coche</h4>
     <h4 class="intro">CREA O ENTRA EN TU GARAJE</h4>
 </div>
@@ -7,15 +7,14 @@
 <div class="login">
     <form action="userLogin.php" method="POST">
         <div>
-            <input type="text" value="Usuario" name='user'>
+            <input type="text" name='user' placeholder="Usuario">
         </div>
-        <br>
-        <br>
+        
         <div>
-            <input type="text" value="Contraseña" name='passsword'>
+            <input type="password" name='passsword' placeholder="Contraseña">
         </div>
         <div>
-            <button type="submit" name="button">Entrar</button>
+            <button class="entar" type="submit" name="button">Entrar en el garaje</button>
         </div>
     
     </form>
